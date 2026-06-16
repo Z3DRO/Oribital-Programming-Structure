7137fa6388c4def1a4335812a735d717cd4639e5
Kriti363 <srivastavakriti171024@gmail.com>
2026-06-16 11:31:21 +0000

    Add outer planets and adjust orbits

--- src/solar/SolarSystemPanel.java
+++ src/solar/SolarSystemPanel.java
@@ -9,8 +9,8 @@ import java.util.List;
 public class SolarSystemPanel extends JPanel implements ActionListener 
 {
 
-    private static final int WIDTH  = 900;
-    private static final int HEIGHT = 700;
+    private static final int WIDTH  = 2160;
+    private static final int HEIGHT = 3420;
 
     private Timer timer;
     private List<Planet> planets;
@@ -26,12 +26,15 @@ public class SolarSystemPanel extends JPanel implements ActionListener
     private void initPlanets() 
     {
         planets = new ArrayList<>();
-        planets.add(new Planet("Mercury", 80,  5,  0.047, 0.0,          new Color(169, 169, 169)));
-        planets.add(new Planet("Venus",   120, 9,  0.018, 1.0,          new Color(230, 190, 100)));
-        planets.add(new Planet("Earth",   170, 10, 0.011, 2.0,          new Color(70,  130, 200)));
-        planets.add(new Planet("Mars",    225, 7,  0.006, 3.0,          new Color(188, 74,  60)));
-        planets.add(new Planet("Jupiter", 305, 20, 0.003, 1.5,          new Color(210, 170, 120)));
-        planets.add(new Planet("Saturn",  395, 16, 0.002, 4.0,          new Color(210, 195, 140)));
+        planets.add(new Planet("Mercury", 60,  5,  0.028, 0.0,          new Color(169, 169, 169)));
+        planets.add(new Planet("Venus",   90, 9,  0.018, 1.0,          new Color(230, 190, 100)));
+        planets.add(new Planet("Earth",   125, 10, 0.011, 2.0,          new Color(70,  130, 200)));
+        planets.add(new Planet("Mars",    160, 7,  0.006, 3.0,          new Color(188, 74,  60)));
+        planets.add(new Planet("Jupiter", 210, 20, 0.003, 1.5,          new Color(210, 170, 120)));
+        planets.add(new Planet("Saturn",  265, 16, 0.002, 4.0,          new Color(210, 195, 140)));
+        planets.add(new Planet("Uranus",  315, 13, 0.0014, 2.5,          new Color(135, 206, 235)));
+        planets.add(new Planet("Neptune",  360, 12, 0.0007, 1.0,          new Color(63, 84, 186)));
+        planets.add(new Planet("Pluto",  400, 3, 0.0005, 0.8,          new Color(188, 164, 140)));
     }
 
     @Override
@@ -101,3 +104,4 @@ private void drawPlanets(Graphics2D g2) {
     }
 }
 }
+
